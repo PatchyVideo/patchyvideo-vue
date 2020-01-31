6d7d692d6474eb9b39d5531c772e163c44663201
freesia <1510033076@qq.com>
2020-01-31 10:37:08 +0800

    修改打包路径

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,15 @@
-
-const webpack = require('webpack')
+const webpack = require("webpack");
 const path = require("path");
 module.exports = {
- 
-publicPath: './',
-outputDir: 'dist',
-    configureWebpack: {
-        plugins: [
-            new webpack.ProvidePlugin({
-                jQuery: 'jquery',
-                $: 'jquery',
-                'windows.jQuery':'jquery'
-            })
-        ]
-    }
-}
+  publicPath: "/",
+  outputDir: "dist",
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        jQuery: "jquery",
+        $: "jquery",
+        "windows.jQuery": "jquery"
+      })
+    ]
+  }
+};
