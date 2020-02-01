0c143ea810c15c18d22e703a42936576cf1b3c90
freesia <1510033076@qq.com>
2020-02-01 15:33:47 +0800

    页面上的编辑标签在列表不可编辑或者未登录的时候隐藏

--- src/views/ListDetail.vue
+++ src/views/ListDetail.vue
@@ -51,10 +51,10 @@
             <p>{{ videolistDesc }}</p>
           </div>
           <!-- 打开Tag编辑页面 -->
-          <div class="edit_box">
+          <div v-if="editable" class="edit_box">
             <el-button type="success">添加视频</el-button>
-            <el-button type="info">编辑</el-button>
-            <el-button type="primary" @click="openEditTags" class="EditTagsButton">编辑标签</el-button>
+            <el-button type="info">编辑列表信息</el-button>
+            <el-button type="primary" @click="openEditTags" class="EditTagsButton">编辑共有标签</el-button>
             <el-button type="danger" @click="dialogVisible = true">删除</el-button>
           </div>
         </div>
@@ -66,7 +66,7 @@
             <div class="re_video">
               <div class="edit">
                 <h1 id="edit_first">{{ index+1 }}</h1>
-                <div id="edit_second">
+                <div v-if="editable" id="edit_second">
                   <Move class="move" :msg="PlaylistItemOp(item,index)"></Move>
                   <!--上移-->
                 </div>
@@ -74,7 +74,13 @@
 
               <img class="re_video_img" :src="'/images/covers/'+item.item.cover_image" />
               <div class="re_video_desc">
-                <el-tooltip class="item" effect="dark" content="在此插入视频" placement="top">
+                <el-tooltip
+                  v-if="editable"
+                  class="item"
+                  effect="dark"
+                  content="在此插入视频"
+                  placement="top"
+                >
                   <router-link
                     :to="{path:'./postvideo',query:getInsertData(item,index)}"
                     class="insert-video"
@@ -101,7 +107,7 @@
                   <i @click="copyVideoLink(index)" class="fa fa-copy fa-lg" style="margin-left:2px"></i>
                 </div>
               </div>
-              <div class="item_end">
+              <div v-if="editable" class="item_end">
                 <SetCover class="set-cover" :msg="PlaylistItemOp(item,index)"></SetCover>
                 <DeleteVideo class="delete-video" :msg="PlaylistItemOp(item,index)"></DeleteVideo>
               </div>
@@ -147,6 +153,10 @@ export default {
           cover: ""
         }
       },
+      // 判断是否登录
+      isLogin: false,
+      // 视频编辑功能是否可见的标志
+      editable: false,
       // 视频列表的名称
       videolistName: "",
       // 视频列表的介绍
@@ -178,7 +188,14 @@ export default {
     };
   },
   computed: {},
-  created() {
+  mounted() {
+    // 查看是否登录
+    if (
+      JSON.stringify(this.$store.state.username) != "null" &&
+      this.$store.state.username != ""
+    ) {
+      this.isLogin = true;
+    }
     this.getVideoList(this.page, this.count);
   },
   methods: {
@@ -226,6 +243,8 @@ export default {
         data: { page: e, page_size: count, pid: this.$route.query.id }
       }).then(result => {
         this.videolistDetail = result.data.data;
+        // 必须是登录且发来的数据是可编辑的才渲染编辑组件
+        this.editable = this.videolistDetail.editable && this.isLogin;
         this.videolistName = this.videolistDetail.playlist.title.english;
         this.videolistDesc = this.videolistDetail.playlist.desc.english;
         this.videolistVideos = this.videolistDetail.videos;
@@ -322,95 +341,78 @@ export default {
     #edit_second {
       //icon
 
-         /deep/  .move-down-box{
-           transform: translateX(-10px);
-          }
+      /deep/ .move-down-box {
+        transform: translateX(-10px);
+      }
 
-          /deep/ .move-up{
+      /deep/ .move-up {
+        .move {
+          flex: 1;
+          height: 100%;
+          font: normal normal normal 14px/1 FontAwesome;
+          font-size: inherit;
+          text-rendering: auto;
+          -webkit-font-smoothing: antialiased;
 
-      .move {
-        flex: 1;
-        height: 100%;
-        font: normal normal normal 14px/1 FontAwesome;
-        font-size: inherit;
-        text-rendering: auto;
-        -webkit-font-smoothing: antialiased;
-
-        /deep/ .move-up {
-          font-size: 40px;
-          height: 40px;
-          color: #808080;
-          transition: all 0.4s ease;
-          /*       position: absolute;
+          /deep/ .move-up {
+            font-size: 40px;
+            height: 40px;
+            color: #808080;
+            transition: all 0.4s ease;
+            /*       position: absolute;
             top: -20px;*/
-          &:hover {
-            color: white;
-            text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228dff,
-              0 0 40px #228dff;
-          }
-          /deep/ .move-down{
-
+            &:hover {
+              color: white;
+              text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228dff,
+                0 0 40px #228dff;
+            }
+            /deep/ .move-down {
               font-size: 40px;
               height: 40px;
               color: #808080;
-              transition: all .4s ease;
+              transition: all 0.4s ease;
 
               /*          position: absolute;
                         bottom: 0px;*/
 
-                &:hover{
-                  color: white;
-                  text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228DFF, 0 0 40px #228DFF;
-                }
+              &:hover {
+                color: white;
+                text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228dff,
+                  0 0 40px #228dff;
+              }
+            }
           }
-
-
-
-        }
-        /deep/ .move-down {
-          font-size: 40px;
-          height: 40px;
-          color: #808080;
-          transition: all 0.4s ease;
-          transform: translateY(120%);
-          /*          position: absolute;
+          /deep/ .move-down {
+            font-size: 40px;
+            height: 40px;
+            color: #808080;
+            transition: all 0.4s ease;
+            transform: translateY(120%);
+            /*          position: absolute;
                         bottom: 0px;*/
 
-          &:hover {
-            color: white;
-            text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228dff,
-              0 0 40px #228dff;
+            &:hover {
+              color: white;
+              text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228dff,
+                0 0 40px #228dff;
+            }
           }
         }
       }
     }
   }
-  }
 }
-  .item_end {
-    width: 10%;
-    display: flex;
-    justify-content: center;
+.item_end {
+  width: 10%;
+  display: flex;
+  justify-content: center;
 
-    .delete-video {
-      flex: 1;
-      height: 30px;
-      font-size: 30px;
-      text-align: center;
-      /deep/ i {
-        &:hover {
-          transition: all 0.4s ease;
-          color: white;
-          text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228dff,
-            0 0 40px #228dff;
-        }
-      }
-    }
-    .set-cover {
-      text-align: center;
-      font-size: 30px;
-      height: 30px;
-      flex: 1;
+  .delete-video {
+    flex: 1;
+    height: 30px;
+    font-size: 30px;
+    text-align: center;
+    /deep/ i {
       &:hover {
         transition: all 0.4s ease;
         color: white;
@@ -419,111 +421,122 @@ export default {
       }
     }
   }
-
-  .content {
-    top: 3px;
-    width: 80%;
-    position: relative;
+  .set-cover {
+    text-align: center;
+    font-size: 30px;
+    height: 30px;
     flex: 1;
+    &:hover {
+      transition: all 0.4s ease;
+      color: white;
+      text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 20px #228dff,
+        0 0 40px #228dff;
+    }
   }
-  .main-page-background-img {
-    background-repeat: no-repeat;
-    min-height: 800px;
-    width: 85%;
-    margin-top: 20px;
-  }
-  .d_t {
-    width: 100%;
-    margin-bottom: 0px;
-    padding-bottom: 5px;
-  }
-  .d_t h2 {
-    padding-top: 20px;
-  }
-  .d_t p {
-    width: 60%;
-    text-align: center;
-    white-space: pre-line;
-    margin: 0px auto;
-  }
-  .d_t img {
-    height: 200px;
-    margin: 10px;
-    background-color: rgba(255, 255, 255, 0);
-  }
+}
 
-  .EditTagsButton {
-/*    width: 70%;*/
-    margin-bottom: 20px;
-  }
+.content {
+  top: 3px;
+  width: 80%;
+  position: relative;
+  flex: 1;
+}
+.main-page-background-img {
+  background-repeat: no-repeat;
+  min-height: 800px;
+  width: 85%;
+  margin-top: 20px;
+}
+.d_t {
+  width: 100%;
+  margin-bottom: 0px;
+  padding-bottom: 5px;
+}
+.d_t h2 {
+  padding-top: 20px;
+}
+.d_t p {
+  width: 60%;
+  text-align: center;
+  white-space: pre-line;
+  margin: 0px auto;
+}
+.d_t img {
+  height: 200px;
+  margin: 10px;
+  background-color: rgba(255, 255, 255, 0);
+}
 
-  .minbox {
-    width: 1200px;
-    margin-left: 12.5px;
-    margin-right: 12.5px;
-    margin-top: 30px;
-    padding:20px 0px;
-  }
+.EditTagsButton {
+  /*    width: 70%;*/
+  margin-bottom: 20px;
+}
 
-  .re_top {
-    width: calc(100% - 20px);
-    margin: 0 auto;
-    margin-top: 20px;
-    padding-bottom: 20px;
-    border-bottom: 3px solid red;
-  }
-  .re_top h5 {
-    margin-right: 5px;
-  }
-  .re_video {
-    text-align: left;
-    /* height: 150px; */
-    margin-left: 10px;
-    margin-right: 10px;
-    margin-top: 20px;
+.minbox {
+  width: 1200px;
+  margin-left: 12.5px;
+  margin-right: 12.5px;
+  margin-top: 30px;
+  padding: 20px 0px;
+}
 
-  }
-  .re_video h1 {
+.re_top {
+  width: calc(100% - 20px);
+  margin: 0 auto;
+  margin-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 3px solid red;
+}
+.re_top h5 {
+  margin-right: 5px;
+}
+.re_video {
+  text-align: left;
+  /* height: 150px; */
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 20px;
+}
+.re_video h1 {
+}
+.re_video_img {
+  display: inline-block;
+  width: 200px;
+  height: 125px;
+  margin-right: 20px;
+  min-width: 200px;
+  min-height: 125px;
+}
+.re_video_desc {
+  width: 850px;
+  display: inline-block;
+  vertical-align: top;
+  white-space: pre-wrap;
+  position: relative;
+}
+.re_video_desc p {
+  font-size: 1rem;
+  line-height: 1.1rem;
+  height: 4.3rem;
+  white-space: pre-wrap;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  /* 使文字变为最多显示4行，多余的使用省略号代替 */
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+}
 
-  }
-  .re_video_img {
-    display: inline-block;
-    width: 200px;
-    height: 125px;
-    margin-right: 20px;
-    min-width: 200px;
-    min-height: 125px;
-  }
-  .re_video_desc {
-    width: 850px;
-    display: inline-block;
-    vertical-align: top;
-    white-space: pre-wrap;
-    position: relative;
-  }
-  .re_video_desc p {
-    font-size: 1rem;
-    line-height: 1.1rem;
-    height: 4.3rem;
-    white-space: pre-wrap;
-    margin-top: 10px;
-    margin-bottom: 10px;
-    /* 使文字变为最多显示4行，多余的使用省略号代替 */
-    -webkit-line-clamp: 4;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-  }
+.page-selector {
+  display: block;
+  text-align: center;
+  margin-top: 20px;
+}
 
-  .page-selector {
-    display: block;
-    text-align: center;
-    margin-top: 20px;
-  }
-
-  .fa-copy:hover {
-    color: olive;
-    cursor: pointer;
-  }
+.fa-copy:hover {
+  color: olive;
+  cursor: pointer;
+}
 </style>
