ae02332b1c7429639020f90dbbd36da5539cb241
suwadaimyojin <248975844@qq.com>
2020-02-21 22:04:18 +0800

    主页视频信息如果不正确，可点击封面重新获取

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -60,6 +60,7 @@
           <p v-if="maxcount">显示 {{ count2 }} / {{ maxcount }} 个视频</p>
           <p v-else>没有搜索到视频</p>
           <el-checkbox v-model="checked">显示已失效视频</el-checkbox>
+          <span style="margin-left: 100px;font-size: 14px;color:#606266;">视频信息不正确，可点击其封面更新。</span>
           <el-select id="select-order" v-model="couponSelected">
             <el-option
               v-for="item in options"
@@ -73,10 +74,14 @@
         <!-- 播放列表正文 -->
         <ul>
           <li class="list-item" v-for="(item, index) in listvideo" :key="item._id.$oid">
-            <div class="video-thumbnail">
+
+            <div class="video-thumbnail"  slot="reference" @click.once="refreshVideo(item)">
               <!--              src="/images/covers/f5da2d4dd9eac171d47eb1100339cbad90e4648556a2f99a.png"-->
               <img :src="'/images/covers/'+item.item.cover_image" width="200px" height="125px" />
+              <div class="Imgcover"></div>
             </div>
+
+
             <div class="video-detail">
               <h4>
                 <router-link
@@ -92,6 +97,7 @@
                   width="16px"
                   style="margin-right:2px"
                 />
+
                 <a :href="item.item.url" :id="'link' + (index)">{{item.item.url}}</a>
                 <i @click="copyVideoLink(index)" class="fa fa-copy fa-lg" style="margin-left:2px"></i>
               </div>
@@ -318,6 +324,26 @@ export default {
           $("html").animate({ scrollTop: 0 }, 100);
         }
       });
+    },
+    refreshVideo:function (item) {
+      console.log(item._id.$oid);
+      this.$axios({
+        method:"post",
+        url:"be/videos/refresh.do",
+        data:{
+          "video_id":
+          item._id.$oid
+        }
+      }).then(res=>{
+        if(res.data.status ==="SUCCEED"){
+          if (this.ifSearch === false) {
+            this.getListVideo(this.page, this.count);
+          }
+          if (this.ifSearch === true) {
+            this.getSearchData(this.page, this.count, this.searchKeyWord);
+          }
+        }
+      })
     }
   },
 
@@ -422,7 +448,22 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+
+  .Imgcover{
+
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 20;
+    top: 0%;
+    transition: all .3s ease;
+    &:hover{
+
+      background-color: rgba(255,255,255,.3);
+    }
+
+  }
 .content {
   background-color: #ffffffc9;
 }
@@ -457,11 +498,27 @@ export default {
 .video-detail {
   height: 125px;
   position: relative;
+  transition: all 0.3s ease;
+  &:hover{
+    /*background-color: rgba(255,255,255,0.3);*/
+    background-color: rgb(244, 244, 245);
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  }
 }
 
 .video-thumbnail {
   margin-right: 20px;
   float: left;
+  position: relative;
+  z-index: 1;
+  img{
+
+  }
+/*  &:hover img{
+      background-color:black;
+  }*/
+    /*  background-color:rgba(255,255,255,.5);*/
+
 }
 
 .list-item {
