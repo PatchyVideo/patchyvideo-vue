e7da0b38feff9e14fd90196ad43a6bfe37fb807e
freesia <1510033076@qq.com>
2020-02-25 15:02:49 +0800

    加载动画优化

--- src/views/Edittag.vue
+++ src/views/Edittag.vue
@@ -67,13 +67,12 @@ export default {
         url: "be/tags/query_categories.do",
         data: {}
       }).then(result => {
-        console.log(result);
         var categories = result.data.data.categories;
         for (var i = 0; i < categories.length; i++) {
           this.tagCategories.push(categories[i].name);
         }
+        this.loading = false;
       });
-      this.loading = false;
     }
   },
   components: { topnavbar, tagDetail, searchTag }
