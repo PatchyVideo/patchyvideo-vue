84f4d0eb15d9296d7bb0d9668559b69f99da5679
wrzrmzx <1510033076@qq.com>
2020-04-30 01:15:14 +0800

    分p功能完成

--- src/components/VideoCompoents/PagesOfVideo.vue
+++ src/components/VideoCompoents/PagesOfVideo.vue
@@ -3,6 +3,8 @@
     功能：视频详情中判断该视频在B站是否有分P
     必要传入参数：
       aid:string类型，视频的av号
+    其他说明：
+      组件会
     更新日志：
       4/28/2020: v1.0
         release;
@@ -43,24 +45,38 @@
     >
       <div
         class="PageItems"
-        v-for="(item,index) in pagesDetail"
+        v-for="(item,index) in pagesDetailOfThisPage"
         :key="index"
-        @click="(()=>{if(!searchingIndex[index].loading)pageOpts(index)})"
+        v-bind:class="{PageItemsActive: item.id == $route.query.id,}"
+        @click="(()=>{if(!item.loading)pageOpts(item.page-1)})"
+        @click.middle="(()=>{if(!item.loading)pageOpts(item.page-1,true)})"
       >
-        <span>P{{ index+1 }}&nbsp;</span>
+        <span>P{{ item.page }}&nbsp;</span>
         <span class="PageName">{{item.part||""}}&nbsp;</span>
-        <span v-if="searchingIndex[index].loading" style="color:#909399">
+        <span v-if="item.loading" style="color:#909399">
           获取索引状态中
           <i class="el-icon-loading"></i>
         </span>
         <div v-else style="display:inline">
-          <span style="color:#67C23A;font-size:12px" v-if="pagesDetail[index].isPosted">存在索引</span>
+          <span style="color:#67C23A;font-size:12px" v-if="item.isPosted">已索引</span>
           <span v-else>
-            <span style="color:#F56C6C;font-size:12px">不存在索引!</span>
+            <span style="color:#F56C6C;font-size:12px">未索引!</span>
             <span class="PostNewIndex">点击添加此分P的索引</span>
           </span>
         </div>
       </div>
+    </div>
+
+    <!-- 分P分页 -->
+    <div style="text-align:center">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="videoPages"
+        :current-page.sync="currentPage"
+        :page-size="pageCount"
+        :hide-on-single-page="true"
+        @current-change="ifPosted"
+      ></el-pagination>
     </div>
   </div>
 </template>
@@ -76,13 +92,14 @@ export default {
     return {
       // 分P的数量
       videoPages: 0,
-      // 分P信息详情
+      // 分P信息详情(所有分P)
       pagesDetail: [],
+      // 当前显示的分P的页数
+      currentPage: 1,
+      // 每一页的分P数量
+      pageCount: 20,
       // 加载分P中的标志
       loading: false,
-      // 每个分P的索引情况的查询加载中的标志
-      // 注意，这里是一个对象数组，使用对象的loading属性进行判断
-      searchingIndex: [],
       // 确认发布索引的提示框
       confirmPosting: false,
       // 需要发布索引的分P对象
@@ -90,6 +107,15 @@ export default {
       // 对于未被索引的视频，发布索引中的标志
       indexPosting: false
     };
+  },
+  computed: {
+    // 分P信息详情（当前页）
+    pagesDetailOfThisPage() {
+      return this.pagesDetail.slice(
+        (this.currentPage - 1) * this.pageCount,
+        Math.min(this.videoPages, this.currentPage * this.pageCount)
+      );
+    }
   },
   created() {},
   mounted() {},
@@ -107,9 +133,9 @@ export default {
           // 为每个分P增加一个URL属性方便查询
           for (const item of this.pagesDetail) {
             item.url = `https://www.bilibili.com/video/av${this.aid}?p=${item.page}`;
-            this.searchingIndex.push({ loading: true });
-            this.ifPosted(item.url, item.page - 1);
+            item.loading = true;
           }
+          this.ifPosted();
         })
         .catch(err => {
           console.log(err);
@@ -117,46 +143,110 @@ export default {
       this.loading = false;
     },
     // 获取分P是否已经被索引的信息
-    async ifPosted(url, index) {
+    async ifPosted() {
       await this.axios({
         method: "post",
-        url: "be/getvideo_url.do",
+        url: "be/getvideo_url_batch.do",
         data: {
-          url: url
+          urls: this.pagesDetailOfThisPage.map(item => item.url)
         }
       })
         .then(res => {
+          console.log(res);
           if (res.data.status == "SUCCEED") {
-            this.pagesDetail[index].isPosted = true;
-            this.pagesDetail[index].detail = res.data.data.video;
+            // 因为pagesDetailOfThisPage是计算属性不能直接修改，这里直接修改pagesDetail
+            this.pagesDetail = this.pagesDetail.map((item, index) => {
+              if (
+                index >= (this.currentPage - 1) * this.pageCount &&
+                index <
+                  Math.min(this.videoPages, this.currentPage * this.pageCount)
+              ) {
+                if (
+                  res.data.data[index - (this.currentPage - 1) * this.pageCount]
+                    .exist
+                ) {
+                  item.isPosted = true;
+                  item.id =
+                    res.data.data[
+                      index - (this.currentPage - 1) * this.pageCount
+                    ].id.$oid;
+                } else {
+                  item.isPosted = false;
+                }
+              }
+              return item;
+            });
           } else {
-            this.pagesDetail[index].isPosted = false;
+            this.pagesDetail = this.pagesDetail.map((item, index) => {
+              if (
+                index >= (this.currentPage - 1) * this.pageCount &&
+                index <
+                  Math.min(this.videoPages, this.currentPage * this.pageCount)
+              ) {
+                item.isPosted = false;
+              }
+              return item;
+            });
           }
         })
         .catch(err => {
-          this.pagesDetail[index].isPosted = false;
           console.log(err);
+          this.pagesDetail = this.pagesDetail.map((item, index) => {
+            if (
+              index >= (this.currentPage - 1) * this.pageCount &&
+              index <
+                Math.min(this.videoPages, this.currentPage * this.pageCount)
+            ) {
+              item.isPosted = false;
+            }
+            return item;
+          });
         });
-      this.searchingIndex[index].loading = false;
+      this.pagesDetail = this.pagesDetail.map((item, index) => {
+        if (
+          index >= (this.currentPage - 1) * this.pageCount &&
+          index < Math.min(this.videoPages, this.currentPage * this.pageCount)
+        ) {
+          item.loading = false;
+        }
+        return item;
+      });
     },
     // 点击分P执行的操作，如果存在分P则跳转至相应的视频详情页面，否则使用当前视频的标签发布索引
-    pageOpts(index) {
-      if (this.pagesDetail[index].isPosted) {
-        this.$router.push({
-          path: "/video",
-          query: { id: this.pagesDetail[index].detail._id.$oid }
-        });
-      } else {
+    pageOpts(index, _blank = false) {
+      if ($("html").scrollTop() && !_blank) {
         // 回到顶部
-        if ($("html").scrollTop()) {
-          //动画效果
-          $("html").animate({ scrollTop: 0 }, 100);
-        }
-        this.$router.push({
+        //动画效果
+        $("html").animate({ scrollTop: 0 }, 100);
+      }
+      if (this.pagesDetail[index].isPosted) {
+        if (_blank) {
+          var routerPath = this.$router.resolve({
+            path: "/video",
+            query: {
+              id: this.pagesDetail[index].id
+            }
+          });
+          window.open(routerPath.href, "_blank");
+        } else
+          this.$router
+            .push({
+              path: "/video",
+              query: {
+                id: this.pagesDetail[index].id
+              }
+            })
+            .catch(err => {});
+      } else {
+        var routerPath = this.$router.resolve({
           path: "/postvideo",
-          query: { use_tags: this.$route.query.id }
+          query: {
+            use_tags: this.$route.query.id,
+            url: this.pagesDetail[index].url,
+            type: this.$parent.myVideoData.video.item.repost_type
+          }
         });
-        console.log("hi");
+        window.open(routerPath.href, "_blank");
 
         // this.postingPage = this.pagesDetail[index];
         // this.confirmPosting = true;
@@ -183,7 +273,7 @@ export default {
 }
 .PagesDetail {
   min-height: 50px;
-  max-height: 150px;
+  max-height: 300px;
   overflow: auto;
 }
 .PageItems {
@@ -191,6 +281,9 @@ export default {
   margin-bottom: 5px;
   margin-top: 5px;
   transition: all 0.4s ease;
+}
+.PageItemsActive {
+  background-color: rgba(223, 223, 223, 0.87);
 }
 .PageItems:hover {
   background-color: rgb(244, 244, 245);
