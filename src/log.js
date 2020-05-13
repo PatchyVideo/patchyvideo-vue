8994cad3ba580727c4cb4f584fad25f5c48343ea
nt_zi_mu <i@ntz.im>
2020-05-13 20:36:29 +0800

    split packages

---
+++ src/log.js
@@ -0,0 +1,79 @@
+console.log("巴瓦鲁魔法服务器启动！");
+console.log(
+  "%cPatchy%cVideo",
+  "color:#9c71b2;font-size:2rem;font-family: Arial,Helvetica,sans-serif;padding: 5px 5px;font-weight: 700;",
+  "color:#ffb458;font-size:2rem;font-family: Arial,Helvetica,sans-serif;padding: 5px 10px;font-weight: 700;"
+);
+console.log("朋友，你知道东方Project吗？我让你加群，好吗？QQ群：757676234");
+/**
+ * Website:
+ * patchyvideo
+ * thvideo
+ * thbwiki
+ *
+ * Staff:
+ * zyddnys
+ * wangbar0133
+ * suwadaimyojin
+ * freesia
+ * paradisewitch
+ * whuihuan
+ * nt_zi_mu
+ * remiio
+ *
+ * thx2:
+ * keisenko
+ * bbleae
+ * kira;rumia
+ * iwasaki_youko
+ * shirokurakana
+ *
+ * other:
+ * github
+ * ipfs
+ * vue
+ * touhou
+ */
+console.log(
+  `                                                         _LL::;cL;
+                                ;Z8,:;L7ixF1jJ7TJ7ijFx77g@g@gX, ;LXC_
+                        :;;Lc4@QpcxL;:'                    ';70,   :XE      :;
+                    ;7iL;:,;Lx'                     ;_        L@  .  K8  ;EEc@S_
+                  LT;' '":;'      _:;;;;;;;;;,:_    D1        ZC  ..  Egg6;' ';LTKj;
+                L7  ''         ,;;"            ';;, J;c_    _JC   ..  p,           ;CF_
+               6G L; __,,:_;ic"                    '8; ;LL;LL; .... T;               CE
+            J7i1LF7:'     ;,    __,LL;;LLLL;;,_      4L ..:....... .ci                 jS
+           ;g:'         LCL;LxsUwaDAIMyOjiNcTj1XX37;  EL;;,:,:,:,;LCO                  ;@
+          cS;        ,,TgL;;;:LKE7L"'''       ''"L1FSE1L_LxTTcTcRRL:Lx;;;,:.          _;g
+        Lx_  _;LO6;  : ,;7c;:K8L:'  ___:___:___   "Lc3i1:  .     L;       ":,;;;     :  6
+      i4L,Lc7c;CXc6L      _LE@;   ,:,:,,:_::,,,;___ ;LpLK ...     ,J:           Kj;:'   Q
+      gE;;;;;,16cixEKxcxcc7;;j   ;:    ' '     L:;;: :4D, ..;:      i7LLL;_      GQR@DSZ8ZD;
+       xR1;,,cX;;,;:,;;;,7T xL  ,i; :Li7;c7c,  ;;     xO    : ,,    'QiiCCZKCSc_  ;,;6X7c1g;
+         LZFLZ        __ S:;O,..7R ,cicTLxTJ7;L;8_c;   :LL;;::''  :cBBleAecTLxGjLL;E  COTLigL
+           ;g:_    ,,;;,;S;JG _;F@ KLLLLcLLLLLX;gL;1x;L  ,DJ;;LKD;L;:;;;: :TT;i@L,;6c  K8Xp ;i0
+           jF ,;::,,,J, LK;R4 ,LJg;6;LLLLLLL;;@cSIwaSAKi_yOukO ;0:  .,,:.: :TLL8DLL:    TcL  FD
+           g ,c,;;;:LXL C14;F:;;JJ48,;;;;;;;;;0R;1i;LLLL; cD;cL,0L c1;:''L7,;LLGC3i,,_   L JX:
+          3i;gL:,;,LTjC SR,_pjFLZL@G;;;;;;;;;Lxg;LDLLLLLL,;84LLLDL Tx17; ,jjLcLDC;L1CCFx _@:
+          8Gg@:_,_;xc;S_8_  JZS7L ZcjxJxJxJTTx1JL 0c;;;LcL;KiS7LQ,:xTL7i; cTTL;RZLcccLX6L1g
+          4L,gJFx;LTL;JR;            '"::;;;;LL   KIRA;RUmiA: iCR ;TLcREmIIOLcLpZLLTLLK8,;'
+                 @gpE7E@L                              ;;LXL  ix LLLLcfrEeSIadRESsTTLRR
+                 gc7iKG___zYdDNYs_                  _          1cc;LLLLLLPaRAdISEWitchKTCp68QRSC;,
+               ;g;"":;1  ig@g@g@g,                 Lc;;,_     OcKiLLLLL;J0JLLCO8ZLTcTTKCS;,,,,;C88E
+             _G8;;;;:Z''"*                        ;gTouHOu@G,   :0Zpp6paTCHyViDEOciTcj1cijcL;,.
+          ;CZGDxL;;;iJ ;;;;:;,:                    '";TCRQg@L   LL;JccJJiTx;7THVIdeo7JiT71pDRpOpESC
+         T@LjJ_ ;L;;KL   ''"::''                          ";::_ j,:;:,:,::_ cgXEThBwIKIxT771i1jFjFx
+         GDZ1;,:L;L;;1                              ;,,;,,;;;;, Rx;;_;_;;_;_E6RR4WHUihUAnxJTixTcxTc
+         g_JZ;;L;LLL;LGQ;                                      ;8LLLLL;L;_;;F;,Z8SSKEiSENkOJJ1111Kj
+       _Zc_ ShiROkURAKaNA,L;,          ____                  ;LKc3,  ;L;L;L;ix,_TD4CxJixJxJJ7x7TJxc
+     L3g;:;;Ec13_:_7@4      ',;c7T;_ _x;Vue;            _LZ0gg;KL4L  L;_;NT_Zi_MugpO7T1p1jJi7J7iiFJ
+   ;Li _67i31c7Q;_:@DG           ,0RgLcR637Z7WANGbar0133ggXL,  4JS6jxcLL;;L38c;;,3@pOJL484GD88QQg@g
+ LSgL;LF4S::,FEj3FpcSpc          _::L4 :,. ;L,_;LZSC;_          cKE0ET1ZEZ;;iL   ;@ggQCLKgGjxJ7xLL'
+x@g@8O4X@Q   @x  ;' 0          _L;; KJ    ;,,IPFS;;,              _;J    j;L1RKL,,i3;Rg@pZSxL:
+ LLccTL; ;CxTO, _  CX     :,CFxJJ8JJL ..:;GIThUB;  J1j L            i   jG7;LLLTcg0:   ;xKSDgg@g@gQ
+            '''"""cgF1j0O;0SSL;;:;:;;:,:,:::LL;;D8pZ18;l;          :i TgQD;,TLLTiLL;gK:
+                   ',L;;;c'    ''' ' ''''       ''  ,@;c;'_         L4  D7xcXTLGL  .;6g,
+                                                      _c4KZDgKJG4xx;G@  g;,,,,DFK3FKKc6J
+                                                             ;;;ciiT7;   g1:LgE;;;LTG0   ,:
+                                                                          E3c7gjExJJcEg  ,;
+                                                                              L6@gDD8D@1`
+);
